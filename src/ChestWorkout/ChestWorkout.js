0ff67688ad1e8a51e5ab0c3ce15429b12c588e28
Amazingspy <amazingspidey348@gmail.com>
2024-02-22 21:33:00 +0530

    Modified

--- src/ChestWorkout/ChestWorkout.js
+++ src/ChestWorkout/ChestWorkout.js
@@ -7,10 +7,19 @@ function ChestWorkout() {
 
 
 
-<div className="">
+<div className="parent">
+
+{/* <h1>Chest Workout</h1>
+    <p>3 sets of 8-12 reps per exercise, or until you can’t anymore.</p><br/>
+    <hr />   */}
 
 
-  
+<div class="card-container1">
+  <div class="centerdiv">
+    <h4>Exercise <br/><input type="text" id="exercise"/></h4>
+  </div>
+</div>
+
 </div>
    
 
