eff16b31ffca79b92d030dc915ff909e6c7071a3
SiddhuGit2810 <dktpg1435d@gmail.com>
2024-04-17 15:08:34 +0530

    siddhu

--- src/Diet/CalorieTracker/CalorieTracker.js
+++ src/Diet/CalorieTracker/CalorieTracker.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from 'react';
+import './CalorieTracker.css';
 
 function CalorieTracker() {
-  return (
-    <div>CalorieTracker</div>
-  )
+
+
+
+
+
+
+
+
+  
+    return (
+        <div>
+            <div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Food</th>
+                            <th>Calorie</th>
+                            <th>Protein</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-body-container">
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        <tr>
+                            <td>2024-04-19</td>
+                            <td>Sprout</td>
+                            <td>45</td>
+                            <td>23</td>
+                        </tr>
+                        
+                        
+                       
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
-export default CalorieTracker
+export default CalorieTracker;
--- src/Diet/DietHomePage/DietHomePage.js
+++ src/Diet/DietHomePage/DietHomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CalorieCalculator from '../CalorieCalculator/CalorieCalculator'
 import CalorieMeter from '../CalorieMeter/CalorieMeter'
 
+import CalorieTracker from '../CalorieTracker/CalorieTracker'
+
 import './DietHomePage.css'
 
 function DietHomePage() {
@@ -11,9 +13,19 @@ function DietHomePage() {
 
     <div>DietHomePage</div>
 <div className="DietContainer">
-<div className="">  <CalorieCalculator/>   </div>
+
+  <div className="First"><div className="">  <CalorieCalculator/>   </div>
 
 <div className=""> <CalorieMeter/> </div>
+ </div>
+
+<div className="Second">
+
+<div className=""> <CalorieTracker/> </div>
+
+</div>
+
+
 
 </div>
 
