c3c21249d254aa75e51f13755f776fb6d8393cc6
SiddhuGit2810 <dktpg1435d@gmail.com>
2024-06-17 18:07:44 +0530

    Siddhu PopUp

--- src/Workout/AbsWorkout/AbsWorkout.js
+++ src/Workout/AbsWorkout/AbsWorkout.js
@@ -12,9 +12,49 @@ function AbsWorkout() {
 
   // const [openModal, setopenModal] = useState(false)
 
+
+
+
+
+  ///add variant popup
+
+
+  const [variantName, setVariantName] = useState('');
+  const [variantType, setVariantType] = useState('');
+  
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await pushCxVariant();
+    setVariantName('');
+    setVariantType('');
+ 
+  };
+
+
+  async function pushCxVariant() {
+    try {
+      const url = "https://fitness-60022916701.development.catalystserverless.in/server/CxVariants/CxVariant";
+      const cxBody = {
+        "variant_Name": variantName,
+        "email": contextEmail.contextemail,
+        "varianttype": variantType
+      };
+      const cxResponse = await axios.post(url, cxBody);
+      console.log(cxResponse);
+    } catch (error) {
+      console.error("Error creating variant", error);
+    }
+  }
+
+  /// add variant popup
   const [isModalOpen, setModalOpen] = useState(false);
 
   const handleOpenModal = () => setModalOpen(true);
+
+ 
   const handleCloseModal = () => setModalOpen(false);
 
   const [cxVariantName, setcxVariantName] = useState([])
@@ -356,15 +396,10 @@ function AbsWorkout() {
               <p class="text-title">Add Variant</p>
 
             </div>
-            <button className="WorkoutTypeCard-button"  onClick={handleOpenModal} > Add info  </button>
-           <Modal open={isModalOpen} onClose={handleCloseModal} />
+            <a className="WorkoutTypeCard-button" href="#popup3"  >Add variant</a>
           </div>
 
-            {/* </div>
-<Modal></Modal>
-            <button className="WorkoutTypeCard-button" onClick={() => setopenModal(true)} > Add info  </button>
-            <Modal open={openModal} onClose={() => setopenModal(false)} />
-          </div> */}
+         
 
 
         </div>
@@ -499,142 +534,46 @@ function AbsWorkout() {
           </div>
         </div>
 
-        {/* <div id="popup2" class="popup-container popup-style-2">
-          <div class="popup-content">
-            <a href="#" class="close">&times;</a>
+       
 
-            <div className={"WorkoutDataContainer"}>
-
-
-              <div className="flip-card-container" id="flipcard" >
-
-                <div className="flip-card">
-                  <div className="flip-card-inner">
-                    <div className="flip-card-front">
-
-                      <p>Previous Workout   <br />  <h6>{prevDate} </h6></p>
-
-                    </div>
-                    <div className="flip-card-back">
-
-
-                      {
-
-                        FitnessData.map((item) => (
+<div  id="popup3" className="popup-container popup-style-2">
 
 
 
-                          <>
-
-
-                            <div className="input-set">
-                              <input type="text" className="input-field" placeholder="Set" value={item.Previous.Set1} />
-                              <input type="text" className="input-field" placeholder="Set" value={item.Previous.Set2} />
-                              <input type="text" className="input-field" placeholder="Set" value={item.Previous.Set3} />
-
-
-
-                            </div>
-                            <div className="input-set">
-                              <input type="text" className="input-field" placeholder="Count" value={item.Previous.Count1} />
-                              <input type="text" className="input-field" placeholder="Count" value={item.Previous.Count2} />
-                              <input type="text" className="input-field" placeholder="Count" value={item.Previous.Count3} />
-
-                            </div>
-                            <div className="input-set">
-
-                              <input type="text" className="input-field" placeholder="Weight" value={item.Previous.Weight1} />
-                              <input type="text" className="input-field" placeholder="Weight" value={item.Previous.Weight2} />
-                              <input type="text" className="input-field" placeholder="Weight" value={item.Previous.Weight3} />
-                            </div>
-
-
-                          </>
-                        ))
-
-                      }
-
-
-
-
-
-
-
-
-
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flip-card">
-                  <div className="flip-card-inner">
-                    <div className="flip-card-front">
-                      <p>Present Workout
-
-                        <h6> {PresentWorkoutName} </h6>
-                        <h6>{error}</h6>
-                      </p>
-                    </div>
-                    <div className="flip-card-back">
-
-
-                      <div className="input-set">
-
-                        <input type="text" className="input-field" placeholder="Set" name="set1" value={formData.set1} onChange={handleChange} />
-                        <input type="text" className="input-field" placeholder="Set" name="set2" value={formData.set2} onChange={handleChange} />
-                        <input type="text" className="input-field" placeholder="Set" name="set3" value={formData.set3} onChange={handleChange} />
-
-                      </div>
-                      <div className="input-set">
-                        <input type="text" className="input-field" placeholder="Count" name="count1" value={formData.count1} onChange={handleChange} />
-                        <input type="text" className="input-field" placeholder="Count" name="count2" value={formData.count2} onChange={handleChange} />
-                        <input type="text" className="input-field" placeholder="Count" name="count3" value={formData.count3} onChange={handleChange} />
-                      </div>
-
-                      <div className="input-set">
-                        <input type="text" className="input-field" placeholder="Weight" name="weight1" value={formData.weight1} onChange={handleChange} />
-                        <input type="text" className="input-field" placeholder="Weight" name="weight2" value={formData.weight2} onChange={handleChange} />
-                        <input type="text" className="input-field" placeholder="Weight" name="weight3" value={formData.weight3} onChange={handleChange} />
-                      </div>
-                      <button className="register-btn" onClick={() => pushData()}  >Add set to workout</button>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="flip-card">
-                  <div className="flip-card-inner">
-                    <div className="flip-card-front">
-                      <p>Future Workout</p>
-                    </div>
-                    <div className="flip-card-back">
-                      <div className="input-set">
-                        <input type="text" className="input-field" placeholder="Count" />
-                        <input type="text" className="input-field" placeholder="Set" />
-                        <input type="text" className="input-field" placeholder="Weight" />
-                      </div>
-                      <div className="input-set">
-                        <input type="text" className="input-field" placeholder="Count" />
-                        <input type="text" className="input-field" placeholder="Set" />
-                        <input type="text" className="input-field" placeholder="Weight" />
-                      </div>
-                      <div className="input-set">
-                        <input type="text" className="input-field" placeholder="Count" />
-                        <input type="text" className="input-field" placeholder="Set" />
-                        <input type="text" className="input-field" placeholder="Weight" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-
-
-
-            </div>
-
+<div className="popup-card">
+          <div className="popup-close">
+            
+          <a href="#" className="close">&times;</a>
+            
+            
           </div>
-        </div> */}
+          <form onSubmit={handleSubmit} className="popup-form">
+            <label htmlFor="variantName">Please enter the variant Name</label>
+            <input
+              type="text"
+              id="variantName"
+              value={variantName}
+              onChange={(e) => setVariantName(e.target.value)}
+              required
+            />
+            <label htmlFor="variantType">Enter the variant type</label>
+            <input
+              type="text"
+              id="variantType"
+              value={variantType}
+              onChange={(e) => setVariantType(e.target.value)}
+              required
+            />
+            <button type="submit" className="popup-button">
+              Create
+            </button>
+          </form>
+        </div>
 
+
+
+
+</div>
 
 
 
--- src/Workout/Modal/Modal.js
+++ src/Workout/Modal/Modal.js
@@ -1,70 +1,70 @@
-import React, { useContext, useState } from 'react';
-import { EmailContext } from '../../Usecontext/UseContext';
-import './Modal.css';
-import axios from 'axios';
+// import React, { useContext, useState } from 'react';
+// import { EmailContext } from '../../Usecontext/UseContext';
+// import './Modal.css';
+// import axios from 'axios';
 
-const Modal = ({ open, onClose }) => {
-  const [variantName, setVariantName] = useState('');
-  const [variantType, setVariantType] = useState('');
-  const contextEmail = useContext(EmailContext) || {};
+// const Modal = ({ open, onClose }) => {
+//   const [variantName, setVariantName] = useState('');
+//   const [variantType, setVariantType] = useState('');
+//   const contextEmail = useContext(EmailContext) || {};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await pushCxVariant();
-    setVariantName('');
-    setVariantType('');
-    onClose();
-  };
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     await pushCxVariant();
+//     setVariantName('');
+//     setVariantType('');
+//     onClose();
+//   };
 
-  async function pushCxVariant() {
-    try {
-      const url = "https://fitness-60022916701.development.catalystserverless.in/server/CxVariants/CxVariant";
-      const cxBody = {
-        "variant_Name": variantName,
-        "email": contextEmail.contextemail,
-        "varianttype": variantType
-      };
-      const cxResponse = await axios.post(url, cxBody);
-      console.log(cxResponse);
-    } catch (error) {
-      console.error("Error creating variant", error);
-    }
-  }
+//   async function pushCxVariant() {
+//     try {
+//       const url = "https://fitness-60022916701.development.catalystserverless.in/server/CxVariants/CxVariant";
+//       const cxBody = {
+//         "variant_Name": variantName,
+//         "email": contextEmail.contextemail,
+//         "varianttype": variantType
+//       };
+//       const cxResponse = await axios.post(url, cxBody);
+//       console.log(cxResponse);
+//     } catch (error) {
+//       console.error("Error creating variant", error);
+//     }
+//   }
 
-  if (!open) return null;
+//   if (!open) return null;
 
-  return (
-    <div className="popup-overlay">
-      <div className="popup-container">
-        <div className="popup-card">
-          <div className="popup-close">
-            <button onClick={onClose}>X</button>
-          </div>
-          <form onSubmit={handleSubmit} className="popup-form">
-            <label htmlFor="variantName">Please enter the variant Name</label>
-            <input
-              type="text"
-              id="variantName"
-              value={variantName}
-              onChange={(e) => setVariantName(e.target.value)}
-              required
-            />
-            <label htmlFor="variantType">Enter the variant type</label>
-            <input
-              type="text"
-              id="variantType"
-              value={variantType}
-              onChange={(e) => setVariantType(e.target.value)}
-              required
-            />
-            <button type="submit" className="popup-button">
-              Create
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="popup-overlay">
+//       <div className="popup-container">
+//         <div className="popup-card">
+//           <div className="popup-close">
+//             <button onClick={onClose}>X</button>
+//           </div>
+//           <form onSubmit={handleSubmit} className="popup-form">
+//             <label htmlFor="variantName">Please enter the variant Name</label>
+//             <input
+//               type="text"
+//               id="variantName"
+//               value={variantName}
+//               onChange={(e) => setVariantName(e.target.value)}
+//               required
+//             />
+//             <label htmlFor="variantType">Enter the variant type</label>
+//             <input
+//               type="text"
+//               id="variantType"
+//               value={variantType}
+//               onChange={(e) => setVariantType(e.target.value)}
+//               required
+//             />
+//             <button type="submit" className="popup-button">
+//               Create
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Modal;
+// export default Modal;
