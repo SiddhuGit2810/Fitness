c44c2914f977f5a5cadb59b08ac1bade7d68ef4f
Amazingspy <amazingspidey348@gmail.com>
2024-06-18 23:39:16 +0530

    css and popup designed

--- src/MySchedule/ProfileCard/ProfileCard.js
+++ src/MySchedule/ProfileCard/ProfileCard.js
@@ -1,61 +1,35 @@
 import React from 'react';
-
-import './ProfileCard.css'
+import './ProfileCard.css';
+import userImage from './images.jpeg';
 
 function ProfileCard() {
-  return (
-    <div className="ProfileCard">
-
-<div className="container">
-            <div className="row">
-                <div className="col-md-offset-4 col-md-4">
-                    <div className="email-signature">
-                        <div className="signature-icon">
-                            <i className="user-icon">👤</i>
-                        </div>
-                        <div className="signature-details">
-                            <h2 className="title">Roshan</h2>
-                            <span className="post">
-                                <img src="" alt="email icon"/> roshan@gmail.com
-                            </span>
-                        </div>
-                        <ul className="signature-content">
-                            <li>
-                                <span className="phone-icon">📞</span>
-                                <br /> Age
-                                <br /> 24
-                            </li>
-                            <li>
-                                <span className="envelope-icon">✉️</span>
-                                <br /> Height
-                                <br /> 172cm
-                            </li>
-                            <li>
-                                <span className="globe-icon">🌐</span>
-                                <br /> Weight
-                                <br /> 60 kg
-                            </li>
-                        </ul>
-                        <ul className="icon">
-                            <li>
-                                <a href="" className="facebook-icon">📘</a>
-                            </li>
-                            <li>
-                                <a href="" className="instagram-icon">📸</a>
-                            </li>
-                            <li>
-                                <a href="" className="twitter-icon">🐦</a>
-                            </li>
-                        </ul>
+    return (
+        <div className="profile-card-container">
+            <div className="left-section">
+                <div className="logo">
+                    <img src={userImage} alt="user" className="profile-image" />
+                </div>
+            </div>
+            <div className="right-section">
+                <h1 className="profile-title">Roshan</h1>
+                <h3 className="profile-email">roshan@gmail.com</h3>
+                <div className="bio">
+                    <div className="profile-item">
+                        <span className="profile-label">Age:</span>
+                        <span className="profile-value">24</span>
+                    </div>
+                    <div className="profile-item">
+                        <span className="profile-label">Height:</span>
+                        <span className="profile-value">172 cm</span>
+                    </div>
+                    <div className="profile-item">
+                        <span className="profile-label">Weight:</span>
+                        <span className="profile-value">60 kg</span>
                     </div>
                 </div>
             </div>
         </div>
-  
-    
-
-    </div>
-  )
+    );
 }
 
-export default ProfileCard
+export default ProfileCard;
