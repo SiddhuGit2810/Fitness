796cec1b253f44480dbf09c435e8d39cdc418863
SiddhuGit2810 <dktpg1435d@gmail.com>
2024-06-18 14:30:31 +0530

    AOs Siddhu

--- src/Diet/DietHomePage/DietHomePage.js
+++ src/Diet/DietHomePage/DietHomePage.js
@@ -1,39 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CalorieCalculator from '../CalorieCalculator/CalorieCalculator'
 import CalorieMeter from '../CalorieMeter/CalorieMeter'
 
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import './DietHomePage.css'
 import TodaysCalorie from '../TodaysCalorie/TodaysCalorie'
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
+
 
 function DietHomePage() {
+
+
+  useEffect(() => {
+    AOS.init();
+  }, [])
+
   return (
 
+
     <>
+      <div >
+        <div>DietHomePage</div>
 
-      <div>DietHomePage</div>
-      <div className="DietContainer">
+        <div className="DietContainer">
 
-        <div className="First"><div className="">  <CalorieCalculator />   </div>
+          <div className="First">
 
-          <div className=""> <CalorieMeter />
-            
+            <div className=""  data-aos="zoom-in"  data-aos-duration="1500" >  <CalorieCalculator />   </div>
+
+            <div className=""  data-aos="zoom-in-up"  data-aos-duration="1500" > <CalorieMeter />
+
+            </div>
           </div>
+          <div className="Second">
+
+            <div className=""> <TodaysCalorie /> </div>
+
+          </div>
+
+
+
+
         </div>
-        <div className="Second">
-
-          <div className=""> <TodaysCalorie/> </div>
-
-        </div>
-
-
 
 
       </div>
-
-
-
 
     </>
   )
